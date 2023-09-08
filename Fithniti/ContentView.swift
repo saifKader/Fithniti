48fa19d2920e7724f3386d130b91145977ccaf8e
Seif eddine <61545317+saifKader@users.noreply.github.com>
2023-09-08 22:59:26 +0100

    Initial Commit

---
+++ Fithniti/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Fithniti
+//
+//  Created by abdelkader seif eddine on 8/9/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Fithniti/FithnitiApp.swift
@@ -0,0 +1,17 @@
+//
+//  FithnitiApp.swift
+//  Fithniti
+//
+//  Created by abdelkader seif eddine on 8/9/2023.
+//
+
+import SwiftUI
+
+@main
+struct FithnitiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
